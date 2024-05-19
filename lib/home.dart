26e4b159d8b5117f5ba967ed430e48eb94906577
sergioIbgz <sergioibagnez@gmail.com>
2024-05-19 12:38:35 -0500

    loading animated

--- lib/home.dart
+++ lib/home.dart
@@ -9,10 +9,70 @@ class Home extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: Center(
-        child: LoadingWidget(width: size.width),
+        child: LoadingAnimated(size: size),
       ),
     );
   }
 }
 
+class LoadingAnimated extends StatefulWidget {
+  final Size size;
 
+  const LoadingAnimated({
+    super.key,
+    required this.size,
+  });
+
+  @override
+  State<LoadingAnimated> createState() => _LoadingAnimatedState();
+}
+
+class _LoadingAnimatedState extends State<LoadingAnimated>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  @override
+  void initState() {
+    controller = AnimationController.unbounded(vsync: this)
+      ..repeat(
+        min: -0.5,
+        max: 1.5,
+        period: const Duration(seconds: 1),
+      );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (BuildContext context, Widget? child) {
+        return ShaderMask(
+          shaderCallback: (Rect bounds) => LinearGradient(
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+            transform: _GradientTransform(controller.value),
+            colors: const [
+              Colors.white10,
+              Colors.white,
+              Colors.white10,
+            ],
+          ).createShader(
+            bounds,
+          ),
+          child: LoadingWidget(
+            width: widget.size.width,
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _GradientTransform extends GradientTransform {
+  final double percent;
+  const _GradientTransform(this.percent);
+  @override
+  Matrix4? transform(Rect bounds, {TextDirection? textDirection}) {
+    return Matrix4.translationValues(bounds.width * percent, 0, 0);
+  }
+}
--- lib/loading_widget.dart
+++ lib/loading_widget.dart
@@ -7,12 +7,13 @@ class LoadingWidget extends StatelessWidget {
   const LoadingWidget({
     super.key,
     required this.width,
-    this.color = const Color(0xFF9F8CC2),
+    this.color = const Color(0xFFB9B9BC),
   }) : height = width * 0.2;
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder(
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: height / 10),
       child: Row(
         mainAxisSize: MainAxisSize.max,
         children: [
@@ -23,13 +24,29 @@ class LoadingWidget extends StatelessWidget {
           ),
           Column(
             mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
-                margin: EdgeInsets.symmetric(horizontal: width * 0.05),
-                height: height / 3,
+                margin: EdgeInsets.symmetric(horizontal: width * 0.05)
+                    .copyWith(bottom: height / 5),
+                height: height / 5,
+                width: width * 0.325,
+                color: color,
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: width * 0.05)
+                    .copyWith(bottom: height / 5),
+                height: height / 5,
                 width: width * 0.65,
                 color: color,
-              )
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: width * 0.05),
+                height: height / 5,
+                width: width * 0.1625,
+                color: color,
+              ),
             ],
           )
         ],
